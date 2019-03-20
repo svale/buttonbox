f0738000ee4e5959f7e5bff2d85f25576522ede6
Svale Fossåskaret <svale@3by5.no>
2019-03-20 22:39:58 +0100

    Fix php 7.2 count error for triggers field types Copy paste from code added in commit #e98bf4b

--- src/fields/Triggers.php
+++ src/fields/Triggers.php
@@ -16,6 +16,8 @@ use supercool\buttonbox\assetbundles\buttonbox\ButtonBoxAsset;
 use Craft;
 use craft\base\ElementInterface;
 use craft\fields\BaseOptionsField;
+use craft\fields\data\OptionData;
+use craft\fields\data\SingleOptionFieldData;
 use craft\helpers\Db;
 use yii\db\Schema;
 use craft\helpers\Json;
@@ -92,8 +94,26 @@ class Triggers extends BaseOptionsField
      */
     public function normalizeValue($value, ElementInterface $element = null)
     {
+        if ( !$value )
+        {
+            $value = $this->defaultValue();
+        }
+        // Normalize to an array
+        $selectedValues = (array)$value;
+        $value = reset($selectedValues) ?: null;
+        $label = $this->optionLabel($value);
+        $value = new SingleOptionFieldData($label, $value, true);
+        $options = [];
+        if ($this->options) {
+            foreach ($this->options as $option) {
+                $selected = in_array($option['value'], $selectedValues, true);
+                $options[] = new OptionData($option['label'], $option['value'], $selected);
+            }
+        }
+        $value->setOptions($options);
         return $value;
     }
+
 
     /**
      * Modifies an element query.
